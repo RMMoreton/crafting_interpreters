84bbe7d07519c7e93830c43d4102dd2747ab85ee
Robert Moreton <robbie.moreton@gmail.com>
2024-04-12 21:19:12 -0400

    Finish up

--- ch01/c/linked_list.c
+++ ch01/c/linked_list.c
@@ -7,15 +7,15 @@
 /*
  * Get the node at the given index. May return the dummy node.
  */
-struct _node* ll_node_at(struct linked_list* l, uint8_t i) {
+struct _node* ll_node_at(struct linked_list* l, size_t i) {
     struct _node* n;
-    if (NULL == l || i > l->len) {
+    if (l == NULL || i > l->len) {
         return NULL;
     }
     // Iterate to the correct node starting at either the head or the tail,
     // whichever is closer.
     if (i <= (l->len+1) / 2) {
-        n = l->head;
+        n = l->head->next;
         for (; i > 0; i--) {
             n = n->next;
         }
@@ -30,143 +30,126 @@ struct _node* ll_node_at(struct linked_list* l, uint8_t i) {
 }
 
 struct linked_list* ll_new() {
-    struct linked_list* l;
     struct _node* n;
+    struct linked_list* l;
+
+    n = (struct _node*)malloc(2 * sizeof(struct _node));
+    if (n == NULL) {
+        return NULL;
+    }
+    n[0].prev = NULL;
+    n[0].next = n + 1;
+    n[0].data = NULL;
+    n[1].prev = n;
+    n[1].next = NULL;
+    n[1].data = NULL;
+
     l = (struct linked_list*)malloc(sizeof(struct linked_list));
-    if (NULL == l) {
+    if (l == NULL) {
+        free(n);
         return NULL;
     }
-    n = (struct _node*)malloc(sizeof(struct _node));
-    if (NULL == n) {
-        free(l);
-        return NULL;
-    }
-    n->next = NULL;
-    n->prev = NULL;
-    n->data = NULL;
-    l->head = n;
-    l->tail = n;
     l->len = 0;
+    l->head = n;
+    l->tail = n + 1;
     return l;
 }
 
-int ll_push_left(struct linked_list* l, char* s) {
+LLE ll_push_left(struct linked_list* l, char* s) {
     return ll_push_at(l, s, 0);
 }
 
-int ll_push_right(struct linked_list* l, char* s) {
+LLE ll_push_right(struct linked_list* l, char* s) {
     return ll_push_at(l, s, l->len);
 }
 
-int ll_push_at(struct linked_list* l, char* s, uint8_t i) {
+LLE ll_push_at(struct linked_list* l, char* s, size_t i) {
     struct _node* n, *n_at;
-    if (NULL == l) {
-        // TODO: return a meaningful error.
-        return -1;
-    }
-    if (NULL == s) {
-        // TODO: return a meaningful error.
-        return -1;
+    if (l == NULL || s == NULL) {
+        return LLE_NULL_ARGUMENT;
     }
     // Can we fit a new node?
-    if (l->len == UINT8_MAX) {
-        // TODO: return a meaningful error.
-        return -1;
+    if (l->len == SIZE_MAX) {
+        return LLE_SIZE;
     }
     // Can we push a node at the requested position?
     if (i < 0 || i > l->len) {
-        // TODO: return a meaningful error.
-        return -1;
+        return LLE_INDEX;
     }
     // Make a new node.
     n = (struct _node*)malloc(sizeof(struct _node));
-    if (NULL == n) {
-        // TODO: return a meaningful error.
-        return -1;
+    if (n == NULL) {
+        return LLE_SYSTEM;
     }
     // Set up the node.
     n->data = strdup(s);
-    if (NULL == n->data) {
+    if (n->data == NULL) {
         // We don't allow pushing NULL data, so this must be a malloc failure.
         free(n);
-        // TODO: return a meaningful error.
-        return -1;
+        return LLE_SYSTEM;
     }
     // Push the node into the list.
     n_at = ll_node_at(l, i);
-    n->next = n_at;
     n->prev = n_at->prev;
+    n->next = n_at;
+    // n_at->prev is non-null because of the dummy nodes and how we implement
+    // the lookup.
+    n_at->prev->next = n;
     n_at->prev = n;
-    if (0 == i) {
-        l->head = n;
-    }
-    if (l->len == i) {
-        l->tail = n;
-    }
-    l->len = l->len + 1;
-    return 0;
+    l->len++;
+    return LLE_OK;
 }
 
-int ll_pop_left(struct linked_list* l, char** s) {
+LLE ll_pop_left(struct linked_list* l, char** s) {
     return ll_pop_at(l, s, 0);
 }
 
-int ll_pop_right(struct linked_list* l, char** s) {
+LLE ll_pop_right(struct linked_list* l, char** s) {
     return ll_pop_at(l, s, l->len - 1);
 }
 
-int ll_pop_at(struct linked_list* l, char** s, uint8_t i) {
+LLE ll_pop_at(struct linked_list* l, char** s, size_t i) {
     struct _node* n;
-    if (NULL == l) {
-        // TODO: return a meaningful error.
-        return -1;
+    if (l == NULL) {
+        return LLE_NULL_ARGUMENT;
     }
     if (i < 0 || i >= l->len) {
-        // TODO: return a meaningful error.
-        return -1;
+        return LLE_INDEX;
     }
     n = ll_node_at(l, i);
-    // Fix references around the node and the head/tail.
-    if (NULL != n->prev) {
-        n->prev->next = n->next;
-    } else {
-        l->head = n->next;
-    }
-    if (NULL != n->next) {
-        n->next->prev = n->prev;
-    } else {
-        l->tail = n->prev;
-    }
-    l->len = l->len - 1;
+    // Pull the node out of the list.
+    n->prev->next = n->next;
+    n->next->prev = n->prev;
+    l->len--;
     // Put the data into s or free it.
-    if (NULL != s) {
+    if (s != NULL) {
         *s = n->data;
     } else {
         // It shouldn't be possible to put NULL into a node's data, but...
-        if (NULL != n->data) {
+        if (n->data != NULL) {
             free(n->data);
         }
     }
     // Free the node and we're done!
     free(n);
-    return 0;
+    return LLE_OK;
 }
 
-int ll_free_all(struct linked_list *l) {
-    struct _node* n, *nt;
-    if (NULL == l) {
-        // TODO: return a meaningful error.
-        return -1;
+LLE ll_free_all(struct linked_list *l) {
+    struct _node* n;
+    if (l == NULL) {
+        return LLE_NULL_ARGUMENT;
     }
-    n = l->head;
-    while (NULL != n) {
-        if (NULL != n->data) {
+    n = l->head->next;
+    while (n != l->tail) {
+        if (n->data != NULL) {
             free(n->data);
         }
-        nt = n->next;
-        free(n);
-        n = nt;
+        n = n->next;
+        free(n->prev);
     }
+    // Also frees l->tail since we allocated them together
+    free(l->head);
     free(l);
-    return 0;
+    return LLE_OK;
 }
--- ch01/c/linked_list.h
+++ ch01/c/linked_list.h
@@ -9,12 +9,23 @@ struct _node {
 struct linked_list {
     struct _node* head;
     struct _node* tail;
-    uint8_t len;
+    size_t len;
 };
 
 /*
+ * Error codes that the various ll_* functions return.
+ */
+typedef enum _ll_errors {
+    LLE_OK,
+    LLE_INDEX,
+    LLE_NULL_ARGUMENT,
+    LLE_SIZE,
+    LLE_SYSTEM,
+} LLE;
+
+/*
  * Allocate a new `struct linked_list`, initialize it, and return a pointer to
- * it.
+ * it. This may return NULL if allocation of the list fails.
  */
 struct linked_list* ll_new();
 
@@ -22,40 +33,40 @@ struct linked_list* ll_new();
  * Push a string onto the front of the linked list. The string is copied into
  * newly allocated space, so the caller can destroy their copy if they want.
  */
-int ll_push_left(struct linked_list* l, char* s);
+LLE ll_push_left(struct linked_list* l, char* s);
 
 /*
  * Push a string onto the back of the linked list. The string is copied into
  * newly allocated space, so the caller can destroy their copy if they want.
  */
-int ll_push_right(struct linked_list* l, char* s);
+LLE ll_push_right(struct linked_list* l, char* s);
 
 /*
  * Push a string at a position of the linked list. The string is copied into
  * newly allocated space, so the caller can destroy their copy if they want.
  */
-int ll_push_at(struct linked_list* l, char* s, uint8_t i);
+LLE ll_push_at(struct linked_list* l, char* s, size_t i);
 
 /*
  * Pop a string from the front of the linked list. Callers must `free()` the
  * resulting char*.
  */
-int ll_pop_left(struct linked_list* l, char** s);
+LLE ll_pop_left(struct linked_list* l, char** s);
 
 /*
  * Pop a string from the back of the linked list. Callers must `free()` the
  * resulting char*.
  */
-int ll_pop_right(struct linked_list* l, char** s);
+LLE ll_pop_right(struct linked_list* l, char** s);
 
 /*
  * Pop a string at a position of the linked list. Callers must `free()` the
  * resulting char*.
  */
-int ll_pop_at(struct linked_list* l, char** s, uint8_t i);
+LLE ll_pop_at(struct linked_list* l, char** s, size_t i);
 
 /*
  * Free all storage that the linked list owns. Keep in mind that the
  * linked_list relinquishes ownership of strings that are popped out.
  */
-int ll_free(struct linked_list* l);
+LLE ll_free(struct linked_list* l);
